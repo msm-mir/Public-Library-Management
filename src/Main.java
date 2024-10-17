7e9747ab51b62d4576a1cb68d0b3a77b29691f71
marble <msm.mirdamad@gmail.com>
2024-10-17 21:01:40 +0330

    menu methods in Main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,36 @@ public class Main {
     public static void main(String[] args) {
         Library library = new Library();
 
+        while (true) {
 
+        }
     }
 
-
+    public void printMenu() {
+        System.out.println("1.Add Member");
+        System.out.println("2.Edit Member");
+        System.out.println("3.Delete Member");
+        System.out.println("4.Show Member");
+        System.out.println("5.Add Book");
+        System.out.println("6.Edit Book");
+        System.out.println("7.Delete Book");
+        System.out.println("8.Show Book");
+        System.out.println("9.Search Member");
+        System.out.println("10.Search Book");
+        System.out.println("11.Borrow Book");
+        System.out.println("12.Return Book");
+        System.out.println("13.Show Members Didn't Return Book");
+        System.out.println("14.Exit");
+    }
+    public void printMemberSearchMenu() {
+        System.out.println("1.Search Member By Name");
+        System.out.println("2.Search Member By Age");
+        System.out.println("3.Search Member By Gender");
+    }
+    public void printBookSearchMenu() {
+        System.out.println("1.Search Book By Name");
+        System.out.println("2.Search Book By Author");
+        System.out.println("3.Search Book By Price");
+        System.out.println("4.Search Book By Borrow Status");
+    }
 }
--- src/Member.java
+++ src/Member.java
@@ -165,7 +165,6 @@ public class Member {
                             readMember(i);
                         }
                     }
-
                 }
             }
         }
