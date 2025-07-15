71219f7f28e8fdaa804b21e5ef684bb3277d520b
marble <msm.mirdamad@gmail.com>
2025-07-15 11:55:40 +0330

    change Library class to interface

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,11 @@
-public class Library {
+interface Library {
+    void save(Entity entity);
+    void update(Entity entity) throws EntityNotFoundException;
+    void delete(Entity entity) throws EntityNotFoundException;
+    Entity[] find(Entity example);
+    void borrow(Member member, Book book);
+    void giveBack(Member member, Book book);
+
     public static Member members[] = new Member[100];
     {
         for (int i = 0; i < 100; i++) {
